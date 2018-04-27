a86d921520dd79981521b99c1667861064bf902d
Nuno Miguel Almeida Soares <nuno.soares@ua.pt>
2018-04-27 02:04:09 +0100

    editar perfil a funcionar com sucesso e atualizar na hora

--- components/PerfilScreen.js
+++ components/PerfilScreen.js
@@ -31,7 +31,6 @@ import axios from "axios";
 
 import HTMLView from "react-native-htmlview";
 
-
 import { ImagePicker } from "expo";
 
 // import { connect } from "react-redux";
@@ -62,10 +61,11 @@ class PerfilScreen extends Component {
       editIsOpen: false,
       name: "",
       descricaoUser: "",
+      tempData: ""
     };
   }
 
-pickImageHandler = async () => {
+  pickImageHandler = async () => {
     let result = await ImagePicker.launchImageLibraryAsync({
       allowsEditing: true,
       aspect: [4, 3]
@@ -91,31 +91,50 @@ pickImageHandler = async () => {
     }
     formData.append("name", name);
     formData.append("descricaoUser", descricaoUser);
-  
- //  console.log(formData);
-    fetch("http://ptua.desenvolvimento/api/useredit/"+user_id, {
+
+    //  console.log(formData);
+    fetch("http://ptua.desenvolvimento/api/useredit/" + user_id, {
       method: "post",
       body: formData
-    }).then(async response => {
-         
-           console.log("resposta"+response);
-          //   await AsyncStorage.setItem('username', response.name);
-          //   await AsyncStorage.setItem('userimg', response.img.toString());
-          //   await AsyncStorage.setItem('descricaoUser', response.descricaoUser);
+    })
+      .then(async res => res.json())
+      .then(async res => {
+        console.log(res);
+        //  console.log(response);
+        await AsyncStorage.setItem("username", res.name);
+        //     await AsyncStorage.setItem('userimg', res.img.toString());
+        await AsyncStorage.setItem("descricaoUser", res.descricaoUser);
 
-  //   this.props.navigation.navigate('PerfilScreen');
-    });
+        this.setState({
+          ua: res.name,
+          du: res.descricaoUser,
+          img: 0
+        });
+
+        this.setState({
+          img: 1
+        });
+      });
 
     this.setState({
-      createNews: false,
+      editIsOpen: false,
       pickedImaged: {},
-      name: '',
-      descricaoUser: '',
+      name: "",
+      descricaoUser: ""
     });
-
   };
 
   async componentWillMount() {
+    /*<Image
+                    source={{
+                      uri:
+                      "https://scontent.fopo1-1.fna.fbcdn.net/v/t1.0-9/16730145_661435734066944_2259181377204724691_n.jpg?_nc_cat=0&oh=33a7b9814af346c739bbe89d8be58669&oe=5B6AF379"
+                      
+                   
+                    }}
+                    style={{ height: 280, width: null, flex: 1 }}
+                  />*/
+
     // axios
     //   .get("http://ptua.desenvolvimento/api/comentarioNoticia/1")
     //   .then(response => {
@@ -143,7 +162,6 @@ pickImageHandler = async () => {
         name: ua,
         descricaoUser: du
       });
-     
     } else {
       alert("esperando");
     }
@@ -220,36 +238,29 @@ pickImageHandler = async () => {
                   </Left>
                 </CardItem>
                 <CardItem cardBody>
-                  {this.state.img == 1 ? <Image
-                    source={{
-                      uri:
-                    "http://ptua.desenvolvimento/storage/users/"+ this.state.id + "/imagem1.jpg"
-                    
-                    }}
-                    style={{ height: 280, width: null, flex: 1 }}
-                  />  : 
-                  <Image
-                    source={{
-                      uri:
-                      "https://scontent.fopo1-1.fna.fbcdn.net/v/t1.0-9/16730145_661435734066944_2259181377204724691_n.jpg?_nc_cat=0&oh=33a7b9814af346c739bbe89d8be58669&oe=5B6AF379"
-                      
-                   
-                    }}
-                    style={{ height: 280, width: null, flex: 1 }}
-                  />
-                  }
-                  
+                  {this.state.img == 1 ? (
+                    <Image
+                      source={{
+                        uri:
+                          "http://ptua.desenvolvimento/storage/users/" +
+                          this.state.id +
+                          "/imagem1.jpg"
+                      }}
+                      style={{ height: 280, width: null, flex: 1 }}
+                    />
+                  ) : (
+                    <Text> </Text>
+                  )}
                 </CardItem>
                 <CardItem>
                   <Body>
-                     <Text style={{ marginTop: 5 }}>
+                    <Text style={{ marginTop: 5 }}>
                       E-mail: {this.state.ue}
                     </Text>
-                    <Text style={{ marginTop: 5, marginBottom: 5 }}>Cargo: {this.state.ut}</Text>
-                    <Text>
-                      {this.state.du}
+                    <Text style={{ marginTop: 5, marginBottom: 5 }}>
+                      Cargo: {this.state.ut}
                     </Text>
-                   
+                    <Text>{this.state.du}</Text>
                   </Body>
                 </CardItem>
                 <CardItem>
@@ -264,87 +275,96 @@ pickImageHandler = async () => {
                   </Right>*/}
                 </CardItem>
               </Card>
-              <Button block info style={{ marginTop: 5 }}
-              onPress={ () => this.setState({
-                editIsOpen: !this.state.editIsOpen
-              })}
+              <Button
+                block
+                info
+                style={{ marginTop: 5 }}
+                onPress={() =>
+                  this.setState({
+                    editIsOpen: !this.state.editIsOpen
+                  })}
               >
                 <Text>Mudar dados</Text>
               </Button>
-             {this.state.editIsOpen ? (
-              <Card>
-                <CardItem>
-                  <Body>
-                    <Form
-                      style={{
-                        height: "100%",
-                        width: "100%",
-                        marginTop: 0,
-                        paddingTop: 0,
-                        marginBottom: 15,
-                        paddingBottom: 0,
-                        backgroundColor: "#ecf0f1"
-                      }}
-                    >
-                      <Item inlineLabel>
-                        <Input
-                          placeholder="Nome..."
-                          ref="name"
-                          onChangeText={name => this.setState({ name })}
-                          value={this.state.name}
-                        />
-                      </Item>
-
-                      <Item inlineLabel>
-                        <Input
-                          placeholder="Descrição..."
-                          ref="descricaoUser"
-                          onChangeText={descricaoUser =>
-                            this.setState({ descricaoUser })}
-                          value={this.state.descricaoUser}
-                        />
-                      </Item>
-
-                      <Button
-                        block
-                        info
+              {this.state.editIsOpen ? (
+                <Card>
+                  <CardItem>
+                    <Body>
+                      <Form
                         style={{
-                          marginTop: 15,
-                          marginRight: 15,
-                          marginLeft: 25
+                          height: "100%",
+                          width: "100%",
+                          marginTop: 0,
+                          paddingTop: 0,
+                          marginBottom: 15,
+                          paddingBottom: 0,
+                          backgroundColor: "#ecf0f1"
                         }}
-                         onPress={this.pickImageHandler}
                       >
-                        <Text>Mudar Imagem Perfil</Text>
-                      </Button>
+                        <Item inlineLabel>
+                          <Input
+                            placeholder="Nome..."
+                            ref="name"
+                            onChangeText={name => this.setState({ name })}
+                            value={this.state.name}
+                          />
+                        </Item>
 
-                      <Button
-                        danger
-                        style={{
-                          marginTop: 45,
-                          marginLeft: 75
-                        }}
-                        //   onPress={() => this.cNoticia(this.state.titulo, this.state.descricao)}
-                      >
-                        <Text>cancelar</Text>
-                      </Button>
-                      <Button
-                        success
-                        style={{
-                          marginTop: -45,
-                          marginLeft: 185
-                        }}
-                        onPress={() => this.UserEdit(this.state.name, this.state.descricaoUser)}
-                      >
-                        <Text>Enviar</Text>
-                      </Button>
-                    </Form>
-                  </Body>
-                </CardItem>
-              </Card>
-             ) : <Text></Text> }
-              
-              
+                        <Item inlineLabel>
+                          <Input
+                            placeholder="Descrição..."
+                            ref="descricaoUser"
+                            onChangeText={descricaoUser =>
+                              this.setState({ descricaoUser })}
+                            value={this.state.descricaoUser}
+                          />
+                        </Item>
+
+                        <Button
+                          block
+                          info
+                          style={{
+                            marginTop: 15,
+                            marginRight: 15,
+                            marginLeft: 25
+                          }}
+                          onPress={this.pickImageHandler}
+                        >
+                          <Text>Mudar Imagem Perfil</Text>
+                        </Button>
+
+                        <Button
+                          danger
+                          style={{
+                            marginTop: 45,
+                            marginLeft: 75
+                          }}
+                          //   onPress={() => this.cNoticia(this.state.titulo, this.state.descricao)}
+                        >
+                          <Text>cancelar</Text>
+                        </Button>
+                        <Button
+                          success
+                          style={{
+                            marginTop: -45,
+                            marginLeft: 185
+                          }}
+                          onPress={() =>
+                            this.UserEdit(
+                              this.state.name,
+                              this.state.descricaoUser
+                            )}
+                        >
+                          <Text>Enviar</Text>
+                        </Button>
+                      </Form>
+                    </Body>
+                  </CardItem>
+                </Card>
+              ) : (
+                <Text />
+              )}
+
               <Button block danger style={{ marginTop: 2 }}>
                 <Text>Apagar conta</Text>
               </Button>
